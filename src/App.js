0333c60393b4d417a8baef055fd20b25f85db3fc
kamila <31627563+InzynierNierobstwa@users.noreply.github.com>
2020-02-12 19:57:01 +0100

    checkbox issue

--- src/App.js
+++ src/App.js
@@ -6,17 +6,26 @@ import Footer from "./component/Footer/Footer";
 
 class App extends React.Component {
   state = {
-    passwordValue: "start",
-    rangeValue: 6
+    passwordValue: "Your new password",
+    rangeValue: 16,
+    isChecked: true
   };
 
   generatePassword = event => {
     event.preventDefault();
-    var length = 8,
+
+    const charsetArray = [
+      "abcdefghijklmnopqrstuvwxyz",
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+      "0123456789",
+      "~!@#$%^&*(){}<>,.?|"
+    ];
+
+    let length = this.state.rangeValue,
       charset =
         "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
       retPass = "";
-    for (var i = 0, n = charset.length; i < length; ++i) {
+    for (let i = 0, n = charset.length; i < length; ++i) {
       retPass += charset.charAt(Math.floor(Math.random() * n));
     }
 
@@ -25,13 +34,30 @@ class App extends React.Component {
     return retPass;
   };
 
+  getRange = event => {
+    event.preventDefault();
+    this.setState({ rangeValue: event.target.value });
+    console.log(this.state.rangeValue);
+  };
+
+  /* here I am - working on checkbox */
+  toggleChange = event => {
+    console.log(event.target.name);
+    this.setState({ isChecked: !this.state.isChecked });
+    console.log(this.state.isChecked);
+  };
+
   render() {
     return (
       <div className={styles.container}>
         <Header name="lock" />
         <Generator
           submitFn={this.generatePassword}
+          rangeFn={this.getRange}
           password={this.state.passwordValue}
+          range={this.state.rangeValue}
+          checked={this.state.isChecked}
+          checkBoxFn={this.toggleChange}
         />
         <Footer />
       </div>
--- src/component/Generator/Generator.js
+++ src/component/Generator/Generator.js
@@ -1,39 +1,65 @@
 import React from "react";
 import styles from "./Generator.module.css";
 
-const Generator = ({ submitFn, ...props }) => {
+const Generator = ({ submitFn, rangeFn, checkBoxFn, ...props }) => {
   return (
     <form className={styles.generatContainer}>
       <div className={styles.generatContainer__input}>
         <label name="length">Password Length:</label>
-        <input type="range" min="6" max="32" name="length"></input>
+        <input
+          type="range"
+          min="5"
+          max="33"
+          onChange={rangeFn}
+          name="length"
+        ></input>
       </div>
       <div className={styles.generatContainer__input}>
-        <label name="symbols">Include Symbols:</label>
-        <input type="checkbox" name="symbols"></input>
-        <label name="symbols"> ( e.g. @#$% ) </label>
+        <label name="Symbols">Include Symbols:</label>
+        <input
+          type="checkbox"
+          name="Symbols"
+          checked={props.checked}
+          onChange={checkBoxFn}
+        ></input>
+        <label name="Symbols"> ( e.g. @#$% ) </label>
       </div>
       <div className={styles.generatContainer__input}>
-        <label name="numbers">Include Numbers:</label>
-        <input type="checkbox" name="numbers"></input>
-        <label name="numbers"> ( e.g. 123456 ) </label>
+        <label name="Numbers">Include Numbers:</label>
+        <input
+          type="checkbox"
+          name="Numbers"
+          checked={props.checked}
+          onChange={checkBoxFn}
+        ></input>
+        <label name="Numbers"> ( e.g. 123456 ) </label>
       </div>
       <div className={styles.generatContainer__input}>
-        <label name="lowercaseChar">Include Lowercase Characters:</label>
-        <input type="checkbox" name="lowercaseChar"></input>
-        <label name="lowercaseChar"> ( e.g. abcdefgh ) </label>
+        <label name="Lower">Include Lowercase Characters:</label>
+        <input
+          type="checkbox"
+          name="Lower"
+          checked={props.checked}
+          onChange={checkBoxFn}
+        ></input>
+        <label name="Lower"> ( e.g. abcdefgh ) </label>
       </div>
       <div className={styles.generatContainer__input}>
-        <label name="uppercaseChar">Include Uppercase Characters:</label>
-        <input type="checkbox" name="uppercaseChar"></input>
-        <label name="uppercaseChar"> ( e.g. ABCDEFGH ) </label>
+        <label name="Upper">Include Uppercase Characters:</label>
+        <input
+          type="checkbox"
+          name="Upper"
+          checked={props.checked}
+          onChange={checkBoxFn}
+        ></input>
+        <label name="Upper"> ( e.g. ABCDEFGH ) </label>
       </div>
       <button className={styles.generatContainer__button} onClick={submitFn}>
         Generate Password
       </button>
       <div className={styles.generatContainer__input}>
         <label name="newPass">Your New Password:</label>
-        <input type="text" value={props.password}></input>
+        <input type="text" value={props.password} readOnly></input>
       </div>
     </form>
   );
